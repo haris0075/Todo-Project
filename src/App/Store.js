01a57fc2c5aaee99bc82ea43e2d3b300e0218ee9
haris0075 <hariszahoor60@gmail.com>
2024-07-03 23:27:13 +0500

    Persist State Between Page Reloads in React using redux-persist

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,6 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from '../features/TodoSlice'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
-export const Store =  configureStore({
-  reducer: todoReducer,
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
+
+
+const Store =  configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
 })
+
+const persistor = persistStore(Store);
+
+export { Store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import {Store} from './App/Store.js'
+import {Store, persistor} from './App/Store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
